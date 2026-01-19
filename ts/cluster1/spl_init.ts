da0db71a228d3cd542d1d15d7553e7e450feee46
Satwik Ghildiyal <satwik15.g@gmail.com>
2026-01-20 03:29:55 +0530

    spl_init done

--- ts/cluster1/spl_init.ts
+++ ts/cluster1/spl_init.ts
@@ -1,6 +1,6 @@
 import { Keypair, Connection, Commitment } from "@solana/web3.js";
-import { createMint } from '@solana/spl-token';
-import wallet from "../turbin3-wallet.json"
+import { createMint, TOKEN_PROGRAM_ID } from '@solana/spl-token';
+import wallet from "./wallet/turbin3-wallet.json"
 
 // Import our keypair from the wallet file
 const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
@@ -13,6 +13,32 @@ const connection = new Connection("https://api.devnet.solana.com", commitment);
     try {
         // Start here
         // const mint = ???
+
+        const mintKeypair = Keypair.generate();
+
+        const mintAddress = await createMint(
+
+            connection,
+
+            keypair,
+
+            keypair.publicKey,
+
+            keypair.publicKey,
+
+            9,
+
+            mintKeypair,
+
+            {
+                commitment: "confirmed"
+            },
+            
+            TOKEN_PROGRAM_ID
+        );
+
+        console.log(`Address of the created token mint is: ${mintAddress.toBase58()} which of course matches with the public key of the mint keypair we passed in to the create mint function: ${mintKeypair.publicKey.toBase58()}.`);
+
     } catch(error) {
         console.log(`Oops, something went wrong: ${error}`)
     }
